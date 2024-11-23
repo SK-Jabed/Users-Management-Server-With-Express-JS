7fe538510a260bed77798fa508ec2eb40bb5a97d
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-24 01:05:49 +0600

    Steps JS File Added and Done

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ app.get("/users", (req, res) => {
     res.send(users);
 })
 
+app.post("/users", (req, res) => {
+    console.log("Post Api Hitting");
+    console.log(req.body);
+})
+
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
 })
